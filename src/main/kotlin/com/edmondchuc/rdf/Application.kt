3559917f41fc2c90f8c5b208a0a8254a94eb6582
Edmond <edmond.chuc@outlook.com>
2022-08-09 01:11:44 +0000

    Change watchPaths scope to classes

--- src/main/kotlin/com/edmondchuc/rdf/Application.kt
+++ src/main/kotlin/com/edmondchuc/rdf/Application.kt
@@ -7,7 +7,7 @@ import com.edmondchuc.rdf.plugins.configureRouting
 
 fun main() {
     // TODO: Fix auto-reload...
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", watchPaths = listOf("rdf-toolbox")) {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", watchPaths = listOf("classes")) {
         configureRouting()
     }.start(wait = true)
 }
